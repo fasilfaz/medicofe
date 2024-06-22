1fdd552c8edc35b42da6156f461847d72296d4e2
fasilfaz <fazsby2001@gmail.com>
2024-06-22 15:35:49 +0530

    debug6

--- src/components/protucted-routes/EasyMethod.jsx
+++ src/components/protucted-routes/EasyMethod.jsx
@@ -5,12 +5,16 @@ const EasyMethod = ({ children}) => {
 
     const navigate = useNavigate();
     const token =  Cookies.get("token");
-    console.log(token, "tokken");
-    useEffect(() => {
-        if(token === undefined || token === null){
-            navigate("/user/login");
-        }
-    },[navigate, token]);
+    // console.log(token, "tokken");
+    // useEffect(() => {
+    //     if(token === undefined || token === null){
+    //         navigate("/user/login");
+    //     }
+    // },[navigate, token]);
+    // return children;
+    if (token === undefined) {
+        navigate("/user/login", {replace: true});
+    }
     return children;
 }
 
