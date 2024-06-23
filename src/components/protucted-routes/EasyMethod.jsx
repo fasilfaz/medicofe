115afc1d1aa5c4f31833507adaeb6e21ed0d6582
fasilfaz <fazsby2001@gmail.com>
2024-06-23 18:51:42 +0530

    commit4

--- src/components/protucted-routes/EasyMethod.jsx
+++ src/components/protucted-routes/EasyMethod.jsx
@@ -6,16 +6,16 @@ const EasyMethod = ({ children}) => {
     const navigate = useNavigate();
     const token =  Cookies.get("token");
     console.log(token, "tokken");
-    useEffect(() => {
-        if(token === undefined || token === null){
-            navigate("/user/login");
-        }
-    },[navigate, token]);
-    return children;
-    // if (token === undefined) {
-    //     navigate("/user/login", {replace: true});
-    // }
+    // useEffect(() => {
+    //     if(token === undefined || token === null){
+    //         navigate("/user/login");
+    //     }
+    // },[navigate, token]);
     // return children;
+    if (token === undefined) {
+        navigate("/user/login", {replace: true});
+    }
+    return children;
 }
 
 export default EasyMethod;
