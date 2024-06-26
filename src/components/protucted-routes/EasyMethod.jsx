e62c106fc74cff00eb3c05aea69a9076eff41c76
fasilfaz <fazsby2001@gmail.com>
2024-06-26 17:47:05 +0530

    edits15

--- src/components/protucted-routes/EasyMethod.jsx
+++ src/components/protucted-routes/EasyMethod.jsx
@@ -1,5 +1,5 @@
 import Cookies from "js-cookie";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 // import { useRecoilState } from "recoil";
 // import { tokenState } from "../../recoil/tokenAtom";
@@ -8,16 +8,16 @@ const EasyMethod = ({ children}) => {
     const navigate = useNavigate();
     const token =  Cookies.get("token");
     console.log(token, "tokken");
-    useEffect(() => {
-        if(token === undefined || token === null){
-            navigate("/user/login");
-        }
-    },[navigate, token]);
-    return children;
-    // if (token === undefined) {
-    //     navigate("/user/login", {replace: true});
-    // }
+    // useEffect(() => {
+    //     if(token === undefined || token === null){
+    //         navigate("/user/login");
+    //     }
+    // },[navigate, token]);
     // return children;
+    if (token === undefined) {
+        navigate("/user/login", {replace: true});
+    }
+    return children;
 }
 
 export default EasyMethod;
