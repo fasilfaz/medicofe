0163979a16cd09d563dd65634a3c5708359f9f04
fasilfaz <fazsby2001@gmail.com>
2024-06-28 15:43:08 +0530

    some edits profile

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Box, Grid } from "@mui/material";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { faHomeAlt, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const backendUrl = "https://medicoba.onrender.com";
 
@@ -97,46 +97,30 @@ const Profile = () => {
                   <p>{user.phoneNumber}</p>
                 </Grid>
               </Grid>
+              <div className="flex flex-row justify-center pb-2">
               <button className="bg-teal-800 px-3 py-1 mt-5 rounded-2xl text-white hover:bg-white hover:text-teal-800 hover:border-2 hover:border-teal-800">
                 <Link to="/user/logout">
                   <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
                 </Link>
               </button>
+              
+              <button className="bg-teal-800 px-3 py-1 mt-5 rounded-2xl text-white hover:bg-white hover:text-teal-800 hover:border-2 hover:border-teal-800">
+                <Link to="/userhome">
+                  <FontAwesomeIcon icon={faHomeAlt} className="mr-2" />
+                </Link>
+              </button>
+              {user.role === 'ADMIN' && (
+                <button className="bg-teal-800 px-3 py-1 mt-5 rounded-2xl text-white hover:bg-white hover:text-teal-800 hover:border-2 hover:border-teal-800">
+                <Link to='/admin/homepage'>
+                  <FontAwesomeIcon icon={faHomeAlt} className="mr-2" />
+                </Link>
+              </button>
+              )}
+              </div>
+              
             </span>
           </Box>
-          {/* <div className="h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white shadow-md rounded-lg overflow-hidden max-w-sm w-full">
-        <div className="bg-green-800 p-4 text-white text-center">
-          <h2 className="text-2xl font-bold">{user.role}</h2>
-        </div>
-        <div className="p-4 text-center">
-          <div className="flex justify-center mb-4">
-            <div className="h-24 w-24 bg-gray-300 rounded-full"></div>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="text-right font-semibold">Name:</div>
-            <div className="text-left">{user.firstName} {user.lastName}</div>
-            <div className="text-right font-semibold">Age:</div>
-            <div className="text-left">{user.age}</div>
-            <div className="text-right font-semibold">Blood Group:</div>
-            <div className="text-left">{user.bloodGroup}</div>
-            <div className="text-right font-semibold">Email:</div>
-            <div className="text-left">{user.email}</div>
-            <div className="text-right font-semibold">Gender:</div>
-            <div className="text-left">{user.gender}</div>
-            <div className="text-right font-semibold">Phone Number:</div>
-            <div className="text-left">{user.phoneNumber}</div>
-          </div>
-          <div className="mt-4">
-            <button className="bg-green-800 text-white py-2 px-4 rounded-full">
-              <svg className="h-6 w-6 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 7l-5 5m0 0l5 5m-5-5h12"></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div> */}
+          
         </div>
       ) : (
         <p>Loading...</p>
