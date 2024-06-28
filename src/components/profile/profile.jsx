932f86ab09e05ff68da0f3ad6b08eade8db26afc
fasilfaz <fazsby2001@gmail.com>
2024-06-28 16:51:33 +0530

    some edits profile4

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -101,7 +101,7 @@ const Profile = () => {
                 <div>
                   <button className="bg-teal-800 px-3 py-1 mt-5 rounded-2xl text-white hover:bg-white hover:text-teal-800 hover:border-2 hover:border-teal-800 m-3">
                     <Link to="/user/logout">
-                      <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+                      <FontAwesomeIcon icon={faSignOutAlt} className="m-2" />
                     </Link>
                   </button>
                 </div>
